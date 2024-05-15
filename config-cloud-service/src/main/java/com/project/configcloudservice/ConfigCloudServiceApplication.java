9851edd3ce33aaa959e9cc5fad3b8cb00474422b
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-05-15 13:41:15 +0200

    initialized config cloud service

---
+++ config-cloud-service/src/main/java/com/project/configcloudservice/ConfigCloudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.configcloudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCloudServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCloudServiceApplication.class, args);
+    }
+
+}
