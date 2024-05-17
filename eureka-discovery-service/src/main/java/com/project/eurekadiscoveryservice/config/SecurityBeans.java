9f4a44fa8f7ccea7f156646ae037de7567bd7ac4
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-05-17 11:13:57 +0200

    added security to the user-service

--- eureka-discovery-service/src/main/java/com/project/eurekadiscoveryservice/config/SecurityBeans.java
+++ eureka-discovery-service/src/main/java/com/project/eurekadiscoveryservice/config/SecurityBeans.java
@@ -7,7 +7,18 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @Configuration
 public class SecurityBeans {
@@ -31,8 +42,31 @@ public class SecurityBeans {
     @Priority(1)
     public SecurityFilterChain mainSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(customizer -> customizer.anyRequest().hasAnyRole("ADMIN", "MANAGER"))
+                .authorizeHttpRequests(customizer -> customizer
+                        .anyRequest().hasAnyRole("ADMIN", "MANAGER"))
+                .oauth2Client(Customizer.withDefaults())
                 .oauth2Login(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public OAuth2UserService<OidcUserRequest, OidcUser> oAuth2UserService() {
+        OidcUserService oidcUserService = new OidcUserService();
+        return userRequest -> {
+            OidcUser oidcUser = oidcUserService.loadUser(userRequest);
+            List<GrantedAuthority> authorities =
+                    Stream.concat(oidcUser.getAuthorities().stream(),
+                                    Optional.ofNullable(oidcUser.getClaimAsStringList("groups"))
+                                            .orElseGet(List::of)
+                                            .stream()
+                                            .filter(role -> role.startsWith("ROLE_"))
+                                            .map(SimpleGrantedAuthority::new)
+                                            .map(GrantedAuthority.class::cast))
+                            .toList();
+
+            System.out.println(authorities);
+
+            return new DefaultOidcUser(authorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+        };
     }
 }
