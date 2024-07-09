0e0aed0263e850a7471f93fb7b1ae7d80d7ea264
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-07-09 11:45:49 +0200

    custom user session that restricts not to have more than 2 sessions

---
+++ custom-event-listener/src/main/java/com/project/session/CustomEventListenerProvider.java
@@ -0,0 +1,59 @@
+package com.project.session;
+
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.events.Event;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
+import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.UserSessionModel;
+
+import java.util.List;
+
+@Slf4j
+public class CustomEventListenerProvider implements EventListenerProvider {
+
+    private final KeycloakSession session;
+
+
+    public CustomEventListenerProvider(KeycloakSession session) {
+        this.session = session;
+    }
+
+
+    @Override
+    public void onEvent(Event event) {
+        if (event.getType() == EventType.LOGIN) {
+            log.info("Login event detected for user: {}", event.getUserId());
+
+            String userId = event.getUserId();
+            List<UserSessionModel> userSessions = session.sessions().getUserSessionsStream(session.getContext().getRealm(),
+                            session.users().getUserById(session.getContext().getRealm(), userId))
+                    .toList();
+
+            System.out.println("userSessions: " + userSessions.size());
+            if (userSessions.size() >= 2) {
+                UserSessionModel oldestSession = userSessions.get(0);
+                for (UserSessionModel userSession : userSessions) {
+                    if (userSession.getStarted() < oldestSession.getStarted()) {
+                        oldestSession = userSession;
+                    }
+                }
+
+
+                session.sessions().removeUserSession(session.getContext().getRealm(), oldestSession);
+                log.info("Oldest session removed for user: {}", userId);
+            }
+        }
+    }
+
+    @Override
+    public void onEvent(AdminEvent adminEvent, boolean b) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
