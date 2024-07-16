bb18bedace7995fe708bbb12ff99135d50e3b691
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-07-16 22:39:28 +0200

    disabled cors

--- gateway-service/src/main/java/com/project/gatewayservice/config/SecurityBeans.java
+++ gateway-service/src/main/java/com/project/gatewayservice/config/SecurityBeans.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityBeans {
@@ -22,6 +24,20 @@ public class SecurityBeans {
                 .oauth2ResourceServer(customizer -> customizer.jwt(Customizer.withDefaults()))
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 
 }
