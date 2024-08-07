5025cf79f4ccdebfbab14c83c804d83209db4070
Ilya_Sedelnikov <illyasedelnikov123@gmail.com>
2024-08-07 16:43:21 +0200

    finished with main project page

--- frontend/task-manager-frontend/src/app/app.routes.ts
+++ frontend/task-manager-frontend/src/app/app.routes.ts
@@ -29,6 +29,12 @@ export const routes: Routes = [
   },
   {
     path: 'main',
-    component: MainPageComponent
+    component: MainPageComponent,
+    children: [
+      {
+        path: 'projects',
+        component: KanbanBoardComponent
+      }
+    ]
   }
 ];
--- frontend/task-manager-frontend/src/app/services/pages/main-page/main-page.component.ts
+++ frontend/task-manager-frontend/src/app/services/pages/main-page/main-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
